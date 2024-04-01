f1f363839f98f2bfc696df008b86211646b5f683
Polvere243 <danielercoli@gmail.com>
2024-04-01 12:08:18 +0200

    seeded database with technologies

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,23 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            [ 'label' => 'html' ],
+            [ 'label' => 'css'],        
+            [ 'label' => 'bootstrap'],        
+            [ 'label' => 'javascript' ],        
+            [ 'label' => 'vue-js' ],        
+            [ 'label' => 'sass' ],        
+            [ 'label' => 'php' ],        
+            [ 'label' => 'sql'],  
+            [ 'label' => 'laravel']  
+        ];        
+        
+
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+            $new_technology->label = $technology['label'];
+            $new_technology->save();
+        }
     }
 }
